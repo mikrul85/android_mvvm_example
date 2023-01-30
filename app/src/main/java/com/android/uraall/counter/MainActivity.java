4240c1bc906f6b6d90da0ce07301326fe75056cd
Mihail Pankratov <mpankratov@rzdit.ru>
2023-01-30 23:21:10 +0300

    final commit

---
+++ app/src/main/java/com/android/uraall/counter/MainActivity.java
@@ -0,0 +1,48 @@
+package com.android.uraall.counter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    MainActivityViewModel model;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        model = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+        model = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+
+        textView = findViewById(R.id.textView);
+
+        LiveData<Integer> countLiveData = model.getCurrentValue();
+
+        countLiveData.observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                textView.setText(String.valueOf(integer));
+            }
+        });
+
+
+    }
+
+    public void decreaseValue(View view) {
+
+        model.getDecreasedValue();
+    }
+
+    public void increaseValue(View view) {
+        model.getIncreasedValue();
+    }
+}
